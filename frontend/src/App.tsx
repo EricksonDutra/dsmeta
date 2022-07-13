722b6b83cd030cd66346a1d487a7013dd8351f88
EricksonDutra <ericksond10@gmail.com>
2022-07-12 20:38:54 -0400

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NoticationButton from './components/NotificationButton'
 function App() {
   return(
+    <>
     <h1>HELLO WORLD</h1>
+    <NoticationButton></NoticationButton>
+    <NoticationButton></NoticationButton>
+    </>
   )
 }
 
