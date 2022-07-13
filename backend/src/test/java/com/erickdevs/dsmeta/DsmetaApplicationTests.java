a91fbea3b0e81e437457182dec585d66ef67fc55
EricksonDutra <ericksond10@gmail.com>
2022-07-12 20:01:55 -0400

    First Commit

---
+++ backend/src/test/java/com/erickdevs/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickdevs.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
