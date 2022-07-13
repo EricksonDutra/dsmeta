579d4195787efcfdc821fafbc6bb70e1eb56bc3d
EricksonDutra <ericksond10@gmail.com>
2022-07-12 21:03:44 -0400

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from './components/Header'
 import NoticationButton from './components/NotificationButton'
+import SalesCard from './components/SalesCard'
 function App() {
   return(
     <>
-    <h1>HELLO WORLD</h1>
-    <NoticationButton></NoticationButton>
-    <NoticationButton></NoticationButton>
+      <Header/>
+      <main>
+        <section id='sales'>
+          <div className='dsmeta-container'>
+            <SalesCard/>      
+          </div>
+
+        </section>
+      </main>
     </>
   )
 }
